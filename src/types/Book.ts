8e8c718834b36a4ea6da5ce30b3461a78537021c
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-07-14 21:17:03 +0600

    Add IBook interface to define book data structure

---
+++ src/types/Book.ts
@@ -0,0 +1,17 @@
+export interface IBook {
+  _id?: string; 
+  title: string;
+  author: string;
+  genre:
+    | "FICTION"
+    | "NON_FICTION"
+    | "SCIENCE"
+    | "HISTORY"
+    | "BIOGRAPHY"
+    | "FANTASY";
+  isbn: number;
+  description?: string;
+  copies: number;
+  available?: boolean;
+  createdAt?: Date;
+}
