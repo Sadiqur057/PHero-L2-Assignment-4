81ffa3b47c8e566093c80f3915f6342fed0ac7cf
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-07-15 21:44:23 +0600

    Refactor baseApi for consistent formatting and improved readability

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,20 +1,68 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
-  reducerPath: 'baseApi',
+  reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3000/api',
+    baseUrl: "http://localhost:3000/api",
   }),
-  tagTypes: ['book'],
+  tagTypes: ["book"],
   endpoints: (builder) => ({
     getBooks: builder.query({
       query: ({ page = 1, limit = 10 }) => ({
-        url: '/books',
+        url: "/books",
         params: { page, limit },
       }),
-      providesTags: ['book'],
+      providesTags: ["book"],
+    }),
+    createBook: builder.mutation({
+      query: (newBook) => ({
+        url: "/books",
+        method: "POST",
+        body: newBook,
+      }),
+      invalidatesTags: ["book"],
+    }),
+    updateBook: builder.mutation({
+      query: ({ id, ...updatedBook }) => ({
+        url: `/books/${id}`,
+        method: "PUT",
+        body: updatedBook.formData,
+      }),
+      invalidatesTags: ["book"],
+    }),
+    deleteBook: builder.mutation({
+      query: (bookId) => ({
+        url: `/books/${bookId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["book"],
+    }),
+    getBookById: builder.query({
+      query: (id) => `/books/${id}`,
+    }),
+    borrowBook: builder.mutation({
+      query: ({ book, quantity, dueDate }) => ({
+        url: `/borrow`,
+        method: "POST",
+        body: { book, quantity, dueDate },
+      }),
+      invalidatesTags: ["book"],
+    }),
+    getSummary: builder.query({
+      query: ({ page = 1, limit = 10 }) => ({
+        url: "/borrow",
+        params: { page, limit },
+      }),
     }),
   }),
 });
 
-export const { useGetBooksQuery } = baseApi;
+export const {
+  useGetBooksQuery,
+  useCreateBookMutation,
+  useDeleteBookMutation,
+  useUpdateBookMutation,
+  useGetBookByIdQuery,
+  useBorrowBookMutation,
+  useGetSummaryQuery,
+} = baseApi;
