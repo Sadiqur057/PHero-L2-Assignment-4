c6285a2f71752521ba697d202f868c616ff05ca2
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-07-15 22:15:08 +0600

    Add loading state handling in EditBook component

--- src/pages/edit-book/page.tsx
+++ src/pages/edit-book/page.tsx
@@ -1,11 +1,15 @@
-import BookForm from '@/components/book-form';
-import { useGetBookByIdQuery } from '@/redux/api/baseApi';
-import { useParams } from 'react-router';
+import BookForm from "@/components/book-form";
+import Loading from "@/components/loading";
+import { useGetBookByIdQuery } from "@/redux/api/baseApi";
+import { useParams } from "react-router";
 
 const EditBook = () => {
   const { id } = useParams<{ id: string }>();
-  const { data } = useGetBookByIdQuery(id);
-  console.log('Edit Book data:', data?.data);
+  const { data, isLoading } = useGetBookByIdQuery(id);
+  console.log("Edit Book data:", data?.data);
+  if (isLoading) {
+    return <Loading fullScreen />;
+  }
   return (
     <div>
       <BookForm
