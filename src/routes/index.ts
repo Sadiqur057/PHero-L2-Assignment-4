dd46584c342c317d9bdc611c28502e884ae97590
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-07-15 21:44:29 +0600

    Refactor route imports for improved organization and consistency

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,15 @@
-import Layout from '@/components/layout/layout';
-import Books from '@/pages/books/page';
-import home from '@/pages/home/home';
-import { createBrowserRouter } from 'react-router';
+import Layout from "@/components/layout/layout";
+import Books from "@/pages/books/page";
+import BorrowBook from "@/pages/borrow-book/page";
+import BorrowSummary from "@/pages/borrow-summary/page";
+import createBook from "@/pages/create-book/page";
+import EditBook from "@/pages/edit-book/page";
+import home from "@/pages/home/home";
+import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     Component: Layout,
     children: [
       {
@@ -13,9 +17,25 @@ export const router = createBrowserRouter([
         Component: home,
       },
       {
-        path: '/books',
-        Component: Books
-      }
+        path: "/books",
+        Component: Books,
+      },
+      {
+        path: "/create-book",
+        Component: createBook,
+      },
+      {
+        path: "/edit-book/:id",
+        Component: EditBook,
+      },
+      {
+        path: "/borrow-book/:id",
+        Component: BorrowBook,
+      },
+      {
+        path: "/borrow-summary",
+        Component: BorrowSummary,
+      },
     ],
   },
 ]);
